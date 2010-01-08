2857ee7fb0d1877b4589ac6a6600218183be9fd7
Jesper Kristensen <cableman@portable.(none)>
2010-01-08 11:46:03 +0100

    Modifiyed layout

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,7 @@
 
 include_once dirname(__FILE__).'/../database/authorize.php.inc';
 include_once dirname(__FILE__).'/../utils/utils.php.inc';
+include_once dirname(__FILE__).'/../utils/layout.php.inc';
 
 include_once dirname(__FILE__).'/../utils/conf.php.inc';
 global $conf;
@@ -33,20 +34,15 @@ try {
   }
 }
 catch (Exception $e) {
-
   // Display login form
   $content = '<div class="spacer">&nbsp;</div>
               <div class="box">
                 <h2 class="alignCenter">Log ind</h2>
                 <form name="login" id="login" action="" method="post">
-                  <div id="field">
-                    <label for="username">Bruger:</label>
-                    <input id="username" name="username" type="text" style="width:200px" autocomplete="off" />
-                  </div>
-                  <div id="field">
-                    <label for="passwd">Adgangskode:<label>
-                    <input id="passwd" name="passwd" type="password" style="width:200px" autocomplete="off" />
-                  </div>
+                  <label for="username">Bruger:</label>
+                  <input id="username" name="username" type="text" autocomplete="off" />
+                  <label for="passwd">Adgangskode:<label>
+                  <input id="passwd" name="passwd" type="password" autocomplete="off" />
                   <div id="feedback"><span>&nbsp;</span></div>
                   <div id="buttons">
                     <input class="button" id="loginBtn" type="button" value="Login" />
@@ -55,7 +51,11 @@ catch (Exception $e) {
               </div>
             <div class="spacer">&nbsp;</div>';
 
-  echo Utils::getPage($content, '<script type="text/javascript" src="'. $conf->getWebroot() .'/js/login.js"></script>');
+  // Build the page
+  $layout = new Layout();
+  $layout->add_JS_file('/js/login.js');
+  $layout->add_content($content);
+  echo $layout;
 }
 
 ?>
